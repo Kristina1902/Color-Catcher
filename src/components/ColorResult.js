7400db24002ecc694966153ed03b2bdbdad79a75
Kristina1902 <kristinastanusic@gmail.com>
2021-11-29 13:55:21 +0100

    changed CSS styles

--- src/components/ColorResult.js
+++ src/components/ColorResult.js
@@ -3,12 +3,12 @@ import React from 'react'
 export const ColorResult = ({ paletteResult}) => {
         return (
             <div>
-                <div style={{display:'flex', flexWrap:'wrap', flexDirection:'row', justifyContent:'space-evenly', width:'100%',marginBottom:'20px'}}> 
+                <div id='colorResultContainer' > 
                 {paletteResult.map((color, index) => {
                     return (
                        
                         <div key={index}>
-                             <div key={index} style={{backgroundColor: color, margin:'20px', width:'100px', height:'100px', borderRadius:'50%'}}> </div>
+                             <div className='colorBox' key={index} style={{backgroundColor: color}}> </div>
                              <p >{color}</p>
                         </div> 
                       
